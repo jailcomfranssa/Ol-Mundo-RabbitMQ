d6f63cbe2b9aa902c6f6312e22cf5bbeba40f94b
jailcomfranssa <jail_son_0@hotmail.com>
2021-04-01 18:11:37 -0300

    first commit

---
+++ src/main/java/com/jf/ifpb/pdist/pratica/Produtor.java
@@ -0,0 +1,30 @@
+package com.jf.ifpb.pdist.pratica;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Produtor {
+    public static void main(String[] args) throws Exception{
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+
+        String NOME_FILA = "filaOla";
+        try(
+
+                Connection connection = connectionFactory.newConnection();
+
+                Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(NOME_FILA, false, false, false, null);
+            String mensagem = "Olá mundo!";
+
+            channel.basicPublish("", NOME_FILA, null, mensagem.getBytes());
+            System.out.println("Enviei mensagem: " + mensagem);
+        }
+    }
+}
+
+
